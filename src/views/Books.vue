c745828cd2cfdd8ad8262a98680ffe64c8e8a0ba
ftaskoski123 <filip.taskoski69@gmail.com>
2024-01-05 15:41:32 +0100

    download books feature

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -3,14 +3,16 @@
     <p>Insert books</p>
     <form @submit.prevent="addBook">
       <label for="author">Author</label>
-      <input type="text" id="author" v-model="author" required />
+      <input class="rounded-lg border-gray-200 border  focus:border-blue-500  focus:outline-none" type="text" id="author" v-model="author" required />
 
       <label for="title">Title</label>
-      <input type="text" id="title" v-model="title" required />
+      <input class="rounded-lg border-gray-200 border  focus:border-blue-500  focus:outline-none" type="text" id="title" v-model="title" required />
 
-      <button type="submit">Submit</button>
+      <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2" type="submit">Submit</button>
     </form>
   </div>
+
+  <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2" @click="downloadBooks">Download Books</button>
 </template>
 
 <script setup lang="ts">
@@ -23,42 +25,21 @@ const author = ref<string>("");
 const title = ref<string>("");
 const user = loadUserFromLocalStorage();
 const id = user ? user.id : null;
-interface ClinicTab {
-  key: string;
-  displayName: string;
-}
-const clinicTabs: ClinicTab[] = [
-  {
-    key: "items",
-    displayName: "Items",
-  },
-  {
-    key: "breeds",
-    displayName: "Breeds",
-  },
-  {
-    key: "colors",
-    displayName: "Colors",
-  },
-  {
-    key: "deathCauses",
-    displayName: "Deceased Reasons",
-  },
-  {
-    key: "declinedReasons",
-    displayName: "Declined Reasons",
-  },
-  {
-    key: "rabiesManufacturers",
-    displayName: "Rabies Manufacturers",
-  },
-  {
-    key: "microchipProviders",
-    displayName: "Microchip Providers",
-  },
-];
 
-const selectedTab = ref<string | null>(null);
+const downloadBooks = () => {
+  axios
+    .get(`${url}api/getbooks/${id}`, { responseType: 'arraybuffer' })
+    .then((response) => {
+      const blob = new Blob([response.data], { type: response.headers['content-type'] });
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'books.txt';
+      link.click();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 const addBook = () => {
   axios
@@ -74,9 +55,5 @@ const addBook = () => {
     });
 };
 
-onMounted(() => {
-  axios.get(`${url}api/getbooks/${id}`).then((response) => {
-    console.log(response.data);
-  });
-});
+
 </script>
