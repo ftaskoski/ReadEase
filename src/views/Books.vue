ca815126d02206512fd3270cd7e01f88dd203f0d
ftaskoski123 <filip.taskoski69@gmail.com>
2024-01-20 19:29:22 +0100

    delete book button style

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -43,7 +43,7 @@
           <tr v-for="book in bookPaginated" :key="book.id">
             <td class="border px-4 py-2">{{ book.author }}</td>
             <td class="border px-4 py-2">{{ book.title }}</td>
-            <td class="border px-4 py-2"><button @click="deleteBook(book.bookId)">Delete book</button></td>
+            <td class="border px-4 py-2"><button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2" @click="deleteBook(book.bookId)">Delete book</button></td>
           </tr>
         </tbody>
       </table>
