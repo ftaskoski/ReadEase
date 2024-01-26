86152e7a7a8e6d92497e957945130c06d5aab974
ftaskoski123 <filip.taskoski69@gmail.com>
2024-01-26 19:33:56 +0100

    cleaner and simplified code

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Data.SqlClient;
-using System.Threading.Tasks;
 using Dapper;
 using WebApplication1.Models;
+using System.Data.SqlClient;
 using System.Text;
+using System.Collections.Generic;
 
 namespace WebApplication1.Controllers
 {
@@ -20,144 +19,99 @@ namespace WebApplication1.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet("downloadbooks/{id}")]
-        public IActionResult DownloadBooks(int id)
+        private SqlConnection GetSqlConnection()
         {
             string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            return new SqlConnection(connectionString);
+        }
 
-            using (var connection = new SqlConnection(connectionString))
+        private IEnumerable<BookModel> QueryBooks(string query, object parameters = null)
+        {
+            using (var connection = GetSqlConnection())
             {
-                string selectQuery = "SELECT Title, Author FROM Books WHERE UserId=@Id;";
-
-                // Use Query to retrieve multiple rows
-                var books = connection.Query<BookModel>(selectQuery, new { Id = id });
-
-                // Check if any books were found
-                if (books != null && books.AsList().Count > 0)
-                {
-                    // Create a StringBuilder to build the plain text content
-                    StringBuilder content = new StringBuilder();
-
-                    // Append book titles and authors to the StringBuilder
-                    foreach (var book in books)
-                    {
-                        content.AppendLine($"Title: {book.Title}, Author: {book.Author}");
-                    }
-
-                    // Return the plain text content as a FileResult
-                    byte[] fileContents = Encoding.UTF8.GetBytes(content.ToString());
-                    return File(fileContents, "text/plain", "books.txt");
-                }
-                else
-                {
-                    return NotFound(); // 404 Not Found if no books were found
-                }
+                return connection.Query<BookModel>(query, parameters);
             }
         }
 
+        private void Execute(string query, object parameters = null)
+        {
+            using (var connection = GetSqlConnection())
+            {
+                connection.Execute(query, parameters);
+            }
+        }
 
+        [HttpGet("downloadbooks/{id}")]
+        public IActionResult DownloadBooks(int id)
+        {
+            string selectQuery = "SELECT Title, Author FROM Books WHERE UserId=@Id;";
+            var books = QueryBooks(selectQuery, new { Id = id });
+
+            if (books?.AsList().Count > 0)
+            {
+                StringBuilder content = new StringBuilder();
+                foreach (var book in books)
+                {
+                    content.AppendLine($"Title: {book.Title}, Author: {book.Author}");
+                }
+
+                byte[] fileContents = Encoding.UTF8.GetBytes(content.ToString());
+                return File(fileContents, "text/plain", "books.txt");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
 
         [HttpGet("getbooks/{id}")]
         public IActionResult GetBooks(int id, int pageNumber = 1, int pageSize = 10)
         {
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                // Calculate the starting row index based on the page number and page size
-                int startIndex = (pageNumber - 1) * pageSize;
-
-                // Use OFFSET-FETCH to implement pagination in SQL Server
-                string getQuery = "SELECT * FROM BOOKS WHERE UserId=@Id ORDER BY BookId OFFSET @startIndex ROWS FETCH NEXT @pageSize ROWS ONLY;";
-
-                var books = connection.Query<BookModel>(getQuery, new { id = id,startIndex=startIndex,pageSize=pageSize });
-                return Ok(books);
-            }
+            int startIndex = (pageNumber - 1) * pageSize;
+            string getQuery = "SELECT * FROM BOOKS WHERE UserId=@Id ORDER BY BookId OFFSET @startIndex ROWS FETCH NEXT @pageSize ROWS ONLY;";
+            var books = QueryBooks(getQuery, new { id = id, startIndex = startIndex, pageSize = pageSize });
+            return Ok(books);
         }
 
-
         [HttpGet("searchbooksall/{id}")]
-        public IActionResult searchBooksAll(int id, string search )
+        public IActionResult SearchBooksAll(int id, string search)
         {
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
-            using (var connection = new SqlConnection(connectionString))
-            {
-
-                string searchQuery = $"SELECT * FROM BOOKS WHERE UserId=@Id AND Author LIKE @Search";
-
-                var book = connection.Query<BookModel>(searchQuery, new { id = id,Search=$"{search}%" });
-
-                return Ok(book);
-            }
-
+            string searchQuery = $"SELECT * FROM BOOKS WHERE UserId=@Id AND Author LIKE @Search";
+            var book = QueryBooks(searchQuery, new { id = id, Search = $"{search}%" });
+            return Ok(book);
         }
 
         [HttpGet("searchbooks/{id}")]
-        public IActionResult searchBooks(int id, string search, int pageNumber = 1, int pageSize = 10)
+        public IActionResult SearchBooks(int id, string search, int pageNumber = 1, int pageSize = 10)
         {
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
-            using (var connection = new SqlConnection(connectionString))
-            {
-                int startIndex = (pageNumber - 1) * pageSize;
-
-                string searchQuery = "SELECT * FROM BOOKS WHERE UserId=@Id AND Author LIKE @Search ORDER BY BookId OFFSET @startIndex ROWS FETCH NEXT @pageSize ROWS ONLY;";
-
-                var book = connection.Query<BookModel>(searchQuery, new { id = id, Search = $"{search}%", startIndex = startIndex, pageSize =pageSize });
-
-                return Ok(book);
-            }
-
+            int startIndex = (pageNumber - 1) * pageSize;
+            string searchQuery = "SELECT * FROM BOOKS WHERE UserId=@Id AND Author LIKE @Search ORDER BY BookId OFFSET @startIndex ROWS FETCH NEXT @pageSize ROWS ONLY;";
+            var book = QueryBooks(searchQuery, new { id = id, Search = $"{search}%", startIndex = startIndex, pageSize = pageSize });
+            return Ok(book);
         }
-
 
         [HttpPost("insertbook/{id}")]
-        public async Task<IActionResult> InsertBook([FromBody] BookModel model, int id)
+        public IActionResult InsertBook([FromBody] BookModel model, int id)
         {
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            string insertQuery = "INSERT INTO Books (Title, Author, UserId) VALUES (@Title, @Author, @UserId);";
+            Execute(insertQuery, new { model.Title, Author = model.Author, UserId = id });
 
-            using (var connection = new SqlConnection(connectionString))
-            {
-                string insertQuery = "INSERT INTO Books (Title, Author, UserId) VALUES (@Title, @Author, @UserId);";
-                await connection.ExecuteAsync(insertQuery, new { model.Title, Author = model.Author, UserId = id });
-
-                return Ok("Book has been added");
-            }
+            return Ok("Book has been added");
         }
-
 
         [HttpGet("getallbooks/{id}")]
-        public IEnumerable<BookModel> getAllBooks(int id)
+        public IEnumerable<BookModel> GetAllBooks(int id)
         {
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
-            using (var connection = new SqlConnection(connectionString))
-            {
-                string getQuery = "SELECT * FROM BOOKS WHERE UserId=@Id";
-                return connection.Query<BookModel>(getQuery, new { Id = id });
-            }
-
+            string getQuery = "SELECT * FROM BOOKS WHERE UserId=@Id";
+            return QueryBooks(getQuery, new { Id = id });
         }
-
 
         [HttpDelete("deletebook/{id}")]
-        public ActionResult deleteBook(int id)
+        public ActionResult DeleteBook(int id)
         {
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                string deleteQuery = "DELETE FROM Books WHERE bookId=@id";
-                connection.Execute(deleteQuery, new { id = id });
-                return Ok();
-
-            }
-
+            string deleteQuery = "DELETE FROM Books WHERE bookId=@id";
+            Execute(deleteQuery, new { id = id });
+            return Ok();
         }
     }
-
-
-
-
-
-
-
 }
