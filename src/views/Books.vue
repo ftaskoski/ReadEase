40a722a920838ecf534afe3e583fdd898fe787ee
Filip Tashkoski <filip.taskoski69@gmail.com>
2024-02-01 11:25:42 +0100

    delete book button ui change

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -73,7 +73,7 @@
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
                 @click="deleteBook(book.bookId)"
               >
-                Delete book
+                Delete book <i class="fa-sharp fa-solid fa-trash"></i>
               </button>
             </td>
           </tr>
@@ -82,10 +82,10 @@
             <td class="border px-4 py-2">{{ book.title }}</td>
             <td class="border px-4 py-2">
               <button
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
+                class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
                 @click="deleteBook(book.bookId)"
               >
-                Delete book
+                Delete book <i class="fa-sharp fa-solid fa-trash"></i>
               </button>
             </td>
           </tr>
