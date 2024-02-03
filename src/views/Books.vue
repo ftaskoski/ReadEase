7a3df2c8963129b0a778191796a8f80948947f70
ftaskoski123 <filip.taskoski69@gmail.com>
2024-02-03 15:46:06 +0100

    ui insert book form change

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -22,18 +22,16 @@
           v-model="title"
           required
         />
-
+        <select required @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="w-full sm:w-auto px-4 py-2.5 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500 transition duration-300">
+          <option value="null" disabled selected hidden>Select a Category</option>
+          <option v-for="category in categories" :key="category" :value="category">{{ category.categoryName }}</option>
+        </select>
         <button
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
           type="submit"
         >
           Submit
         </button>
-        Select a Category
-        <select required @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="w-full sm:w-auto px-4 py-2.5 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500 transition duration-300">
-          <option value="null" disabled selected hidden>Select a Category</option>
-          <option v-for="category in categories" :key="category" :value="category">{{ category.categoryName }}</option>
-        </select>
       </form>
       <button
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
