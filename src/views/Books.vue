61a33339fd207035c8a550c830cc2570f167ad8b
ftaskoski123 <filip.taskoski69@gmail.com>
2024-02-03 11:29:52 +0100

    placeholder for selecting a category

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -30,7 +30,8 @@
           Submit
         </button>
         Select a Category
-        <select @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="w-full sm:w-auto px-4 py-2.5 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500 transition duration-300">
+        <select required @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="w-full sm:w-auto px-4 py-2.5 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500 transition duration-300">
+          <option value="null" disabled selected hidden>Select a Category</option>
           <option v-for="category in categories" :key="category" :value="category">{{ category.categoryName }}</option>
         </select>
       </form>
