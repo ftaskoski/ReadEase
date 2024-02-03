a4146e269891329bcf40d654aa1d45dbaa62416e
ftaskoski123 <filip.taskoski69@gmail.com>
2024-02-03 15:40:44 +0100

    admin feature to add categories

--- Controllers/Categories.cs
+++ Controllers/Categories.cs
@@ -34,10 +34,22 @@ namespace WebApplication1.Controllers
         }
 
         [HttpGet("categories")]
-        public IEnumerable<CategoriesModel> getCategorie()
+        public IEnumerable<CategoriesModel> getCategories()
         {
             string getQuery = "SELECT * FROM CATEGORIES";
             return QueryCategories(getQuery);
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("insertcategory")]
+        public void insertCategories(CategoriesModel category)
+        {
+            using(var connection = GetSqlConnection())
+            {
+                string insertQuery = "INSERT INTO Categories (CategoryName) VALUES (@CategoryName)";
+                connection.Execute(insertQuery, new { CategoryName=category.CategoryName });
+            }
         }
     }
 }
