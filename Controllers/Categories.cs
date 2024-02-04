e663e2e33dc6c65e967c3edfab30f472ef8aa141
ftaskoski123 <filip.taskoski69@gmail.com>
2024-02-04 02:57:11 +0100

    pagination on checked books fix

--- Controllers/Categories.cs
+++ Controllers/Categories.cs
@@ -55,12 +55,22 @@ namespace WebApplication1.Controllers
 
 
         [HttpGet("checked/{id}")]
-        public IEnumerable<BookModel> getChecked(int id, [FromQuery] string categories)
+        public IEnumerable<BookModel> getChecked(int id, [FromQuery] string categories,int pageNumber=1, int pageSize=10)
+        {
+            int startIndex = (pageNumber - 1) * pageSize;
+            var categoriesList = categories.Split(',').Select(Int32.Parse).ToList();
+            var connection = GetSqlConnection();
+            var getQuery = "SELECT * FROM Books WHERE UserId=@Id AND CategoryId IN @categories ORDER BY BookId OFFSET @startIndex ROWS FETCH NEXT @pageSize ROWS ONLY";
+            return connection.Query<BookModel>(getQuery, new { id, categories = categoriesList,startIndex,pageSize });
+        }
+
+        [HttpGet("checkedall/{id}")]
+        public IEnumerable<BookModel> getCheckedAll(int id, [FromQuery] string categories)
         {
             var categoriesList = categories.Split(',').Select(Int32.Parse).ToList();
             var connection = GetSqlConnection();
-            var getQuery = "SELECT * FROM Books WHERE UserId=@Id AND CategoryId IN @categories";
-            return connection.Query<BookModel>(getQuery, new { id, categories = categoriesList });
+            var getQuery = "SELECT * FROM Books WHERE UserId=@Id AND CategoryId IN @categories ";
+            return connection.Query<BookModel>(getQuery, new { id, categories = categoriesList, });
         }
 
     }
