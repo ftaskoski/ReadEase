bfc5a8a28e3b6777765dd50f52ff13a4ad45ce5e
ftaskoski123 <filip.taskoski69@gmail.com>
2024-02-04 01:37:55 +0100

    search books by category feature

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -55,6 +55,11 @@
       />
     </div>
 
+    <div v-for="category in categories">
+      <input v-model="checkedCategories" @change="check" type="checkbox" :value="category.categoryId"  />
+      <label>{{ category.categoryName }}</label>
+    </div>
+
     <div class="flex justify-center items-center">
       <table class="table-auto w-full">
         <thead>
@@ -79,7 +84,20 @@
               </button>
             </td>
           </tr>
-          <tr v-else v-for="book in bookPaginated" :key="book.id">
+          <tr v-else-if="bookPaginated" v-for="book in bookPaginated" :key="book.id">
+            <td class="border px-4 py-2">{{ book.author }}</td>
+            <td class="border px-4 py-2">{{ book.title }}</td>
+            <td class="border px-4 py-2">{{ getCategoryName(book.categoryId) }}</td>
+            <td class="border px-4 py-2">
+              <button
+                class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
+                @click="deleteBook(book.bookId)"
+              >
+                Delete book <i class="fa-sharp fa-solid fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+          <tr v-if="checkedBooks " v-for="book in checkedBooks">
             <td class="border px-4 py-2">{{ book.author }}</td>
             <td class="border px-4 py-2">{{ book.title }}</td>
             <td class="border px-4 py-2">{{ getCategoryName(book.categoryId) }}</td>
@@ -333,5 +351,24 @@ onMounted(() => {
   getBooks();
   getnybooks();
 });
+
+const checkedCategories = ref<number[]>([]);
+const checkedBooks = ref<any[]>([]);
+  function check() {
+  axios.get(`${url}api/checked/${id}`, {
+    params: {
+      categories: checkedCategories.value.join(',')
+    },
+    withCredentials: true
+  })
+  .then((response) => {
+    console.log(response.data);
+    checkedBooks.value = response.data;
+
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
 
 </script>
