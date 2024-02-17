42ec460a55ef4024af1323bf7a1f40cb1a95edcb
ftaskoski123 <filip.taskoski69@gmail.com>
2024-02-17 18:24:57 +0100

    card ui change

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <!-- Card.vue -->
 <template>
-    <div class="flex justify-center items-center h-screen">
+    <div class="flex ">
       <div class="relative mx-auto w-full max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
     <slot></slot>
       </div>
