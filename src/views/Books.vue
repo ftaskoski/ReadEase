da421b238c329a0af53f7d204fb647ee08038cf2
ftaskoski123 <filip.taskoski69@gmail.com>
2024-02-18 12:53:02 +0100

    insert books ui change

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -3,6 +3,7 @@
     <div class="order-1 sm:order-2 sm:ml-64 p-4 ">
       <div>
     <div >
+
       <Card class="max-w-md flex justify-center items-center">
       <p class="text-3xl font-semibold text-gray-900 flex justify-center pb-2" >INSERT BOOKS</p>
       <form @submit.prevent="addBook">
@@ -37,12 +38,12 @@
         
         </div>
         <div class="relative inline-block w-full mt-2">
-  <select @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
-    <option value="null" disabled selected hidden>Select a Category</option>
-    <option   v-for="category in categories" :key="category" :value="category">{{ category.categoryName }}</option>
-  </select>
-  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 7l3-3 3 3m0 6l-3 3-3-3" /></svg>
+        <select @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+          <option value="null" disabled selected hidden>Select a Category</option>
+          <option   v-for="category in categories" :key="category" :value="category">{{ category.categoryName }}</option>
+      </select>
+  <div class="pointer-events-none absolute text-sm inset-y-0 right-0 flex items-center px-2 text-gray-700">
+    <span class="flex items-center">&#9660;</span>
   </div>
 </div>
 
@@ -55,6 +56,7 @@
         </button>
       </form>
     </Card>
+
       <button
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
         @click="downloadBooks"
