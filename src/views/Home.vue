fc08207f85ad6fa341ec18633ffb715d2316e1a3
Filip Tashkoski <filip.taskoski69@gmail.com>
2024-02-21 12:05:29 +0100

    ui update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-   <div class="order-1 sm:order-2 sm:ml-64 p-4 ">
-  <div class="relative group flex flex-wrap justify-center items-center">
+   <div class="order-1 sm:order-2 sm:ml-64 p-4 m">
+  <div class="relative lg:ml-16 group flex flex-wrap justify-center items-center">
     <div v-for="book in books" :key="book.rank" class="w-1/3 p-2">
       <img :src="book.book_image" :alt="book.title" class="" />
     </div>
@@ -11,18 +11,13 @@
 <script setup lang="ts">
 
 import axios from "axios";
-import { useRouter,} from "vue-router";
 import {ref , onMounted } from 'vue';
-
-
 
 const books = ref<any[]>([]);
 const getnybooks = async () => {
   const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=egM8BbGFHGODa7lpiV0SFCAKhJlzG72G`;
   const response = await axios.get(url).then((response)=>{
-    console.log(response.data.results.books);
     books.value=response.data.results.books;
-    console.log(books.value);
   })
 }
 onMounted(()=>{
