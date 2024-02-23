d58d26448c536d5956105e0064c2f772f3dc0222
Filip Tashkoski <filip.taskoski69@gmail.com>
2024-02-23 12:04:56 +0100

    overflow fix

--- src/components/BookTable.vue
+++ src/components/BookTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center">
+  <div class="flex justify-center items-center overflow-auto">
     <table class="table-auto w-full">
       <thead>
         <tr>
