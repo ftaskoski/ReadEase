57f94b46b8e198b18204a7a683032797b7160819
Filip Tashkoski <filip.taskoski69@gmail.com>
2024-02-23 12:37:38 +0100

    ui fix sidebar open

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <!-- Card.vue -->
 <template>
     <div class="flex ">
-      <div class="relative mx-auto w-full max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
+      <div class="relative mx-auto w-full max-w-md  px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
     <slot></slot>
       </div>
     </div>
--- src/views/Books.vue
+++ src/views/Books.vue
@@ -39,7 +39,7 @@
         </div>
 
 <div class="relative inline-block w-full mt-2 text-gray-500">
-  <select @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="peer block appearance-none  w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+  <select @change="getCategoryIdFromSelectedCategory" v-model="selectedCategory" class="peer block appearance-none  w-full  border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
     <option value="null" disabled selected hidden>Select a Category</option>
     <option v-for="category in categories" :key="category" :value="category">{{ category.categoryName }}</option>
   </select>
