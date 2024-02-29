3a477117dc78d99d9b409b613a70a2b3507ff9c2
Filip Tashkoski <filip.taskoski69@gmail.com>
2024-02-29 13:02:32 +0100

    using the new book table component

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -96,89 +96,10 @@
   </div>
 
 
-<div class="flex justify-center items-center overflow-auto">
-    <table class="table-auto w-full">
-      <thead>
-        <tr>
-          <th class="px-4 py-2">Author</th>
-          <th class="px-4 py-2">Title</th>
-          <th class="px-4 py-2">Category</th>
-          <th class="px-4 py-2">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="book in books" :key="book.bookId">
-          <td class="border px-4 py-2">{{ book.author }}</td>
-          <td class="border px-4 py-2">{{ book.title }}</td>
-          <td class="border px-4 py-2">{{ getCategoryName(book.categoryId) }}</td>
-          <td class="border px-4 py-2">
-            <button
-              class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
-              @click="openModal(book.bookId)"
-            >
-              Delete book <i class="fa-sharp fa-solid fa-trash"></i>
-            </button>
-          </td>
-          <td class="border px-4 py-2">
-            <button
-              class="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
-              @click="openEditModal(book.bookId)"
-            >
-              Edit book <i class="fa-sharp fa-solid fa-pen-to-square"></i>
-          </button>
-          </td>
-          <div v-if="showEditModal" class="fixed  inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-      <div class="relative w-auto max-w-md  mx-auto my-6">
-        <!-- Content -->
-        <div class="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
-          <!-- Header -->
-          <div class="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
-            <h3 class="text-3xl font-semibold">
-             Edit book
-            </h3>
-            <button
-              class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-            >
-              <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">×</span>
-            </button>
-          </div>
-          <!-- Body -->
-          <div class="relative p-6 flex-auto">
-            <input type="text"   v-model="newTitle" class="mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Title" >
-            <input type="text" v-model="newAuthor" class="mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Author" >
-            <select v-model="newCategoryId"  class="mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-              <option value="0" disabled selected hidden>Select a Category</option>
-              <option value="0">Select category</option>
-              <option  v-for="category in categories" :key="category.categoryId" :value="category.categoryId">{{ category.categoryName }}</option>
-            </select>
-          </div>
-          <!-- Footer -->
-          <div class="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
-            <button
-              class="text-blue-500 rounded-full hover:bg-gray-200 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-              @click="updateBook()"
-            >
-              Update
-            </button>
-            <button
-            @click="closeEditModal()"
-              class="text-blueGray-500 rounded-full hover:bg-gray-200 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-        </tr>
-      </tbody>
-
-   <DeleteModal :showModal="showModal" :closeModal="closeModal" :deleteBook="deleteBook"/>
+  <BookTable :books="books" :getCategoryName="getCategoryName" :openModal="openModal" :showModal="showModal" :deleteBook="deleteBook" :closeModal="closeModal"    :categories="categories" :currPage="currPage" :totalPages="totalPages" :newAuthor="newAuthor" :newTitle="newTitle" :newCategoryId="newCategoryId" :updateBook="updateBook" :openEditModal="openEditModal" :showEditModal="showEditModal" :closeEditModal="closeEditModal"  @update:newTitle="val => newTitle = val"
+    @update:new-author="val => newAuthor = val"
+    @update:new-category-id="val => newCategoryId = val"  />  
     
-    </table>
-  </div>
 
 
 <div class="flex justify-center items-center mt-4">
@@ -225,6 +146,7 @@ import { ref, onMounted, computed, watch } from "vue";
 import { useRouter } from "vue-router";
 import Card from "@/components/Card.vue";
 import DeleteModal from "@/components/DeleteModal.vue";
+import BookTable from "@/components/BookTable.vue";
 const router = useRouter();
 const url = "https://localhost:7284/";
 let debounceTimer = 0;
