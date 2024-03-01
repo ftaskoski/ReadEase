d865a30da40045cb91943f8b61d2afda4083e709
ftaskoski123 <filip.taskoski69@gmail.com>
2024-03-02 00:59:38 +0100

    hover effect on table

--- src/components/BookTable.vue
+++ src/components/BookTable.vue
@@ -10,26 +10,25 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="book in books" :key="book.bookId">
+        <tr class="hover:bg-gray-100" v-for="book in books" :key="book.bookId">
           <td class="border px-4 py-2">{{ book.author }}</td>
           <td class="border px-4 py-2">{{ book.title }}</td>
           <td class="border px-4 py-2">{{ getCategoryName(book.categoryId) }}</td>
-          <td class="border px-4 py-2">
+          <td class="border px-4 py-2 ">
             <button
-              class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
+              class="text-white  bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5  py-2.5 text-center mt-2"
               @click="openModal(book.bookId)"
             >
               Delete book <i class="fa-sharp fa-solid fa-trash"></i>
             </button>
-          </td>
-          <td class="border px-4 py-2">
             <button
               class="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
               @click="openEditModal(book.bookId)"
             >
               Edit book <i class="fa-sharp fa-solid fa-pen-to-square"></i>
           </button>
-          </td>      
+          </td>
+           
 
         </tr>
       </tbody>
