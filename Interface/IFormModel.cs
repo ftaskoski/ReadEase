4d718d045ad102a31d974210070813bc58a1758e
ftaskoski123 <filip.taskoski69@gmail.com>
2024-03-06 00:34:20 +0100

    new form modal interface

---
+++ Interface/IFormModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadEase_C_.Interface
+{
+    public interface IFormModel
+    {
+        int Id { get; set; }
+        string Password { get; set; }
+        string? Role { get; set; }
+        string Username { get; set; }
+    }
+}
--- Models/FormModel.cs
+++ Models/FormModel.cs
@@ -1,6 +1,8 @@
-﻿namespace WebApplication1.Models
+﻿using ReadEase_C_.Interface;
+
+namespace WebApplication1.Models
 {
-    public class FormModel
+    public class FormModel : IFormModel
     {
         public int Id { get; set; }
         public string Username { get; set; }
