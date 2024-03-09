055d1f19855373c4093630f17bd41358523aadc4
ftaskoski123 <filip.taskoski69@gmail.com>
2024-03-09 17:22:50 +0100

    new admin icon

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -88,7 +88,7 @@
                 class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <i
-                  class="fa fa-user-tie flex-shrink-0 w-5 mt-0.5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  class="fa fa-user-shield flex-shrink-0 w-5 mt-0.5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                   aria-hidden="true"
                 ></i>
                 <span class="flex-1 ms-3 whitespace-nowrap mb-1">Admin</span>
