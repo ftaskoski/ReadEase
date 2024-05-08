6b8d1d86e2d75c8fe195b17b842c5137bf340005
ftaskoski123 <filip.taskoski69@gmail.com>
2024-05-08 13:31:12 +0200

    fixed profile picture not loading on refresh bug

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {AuthStatus } from '@/store/authStore';
+import { getProfilePicture } from './store/picStore';
 
 const app = createApp(App)
 
 
 
 await AuthStatus();
-
+getProfilePicture();
 app.use(router)
 
 app.mount('#app')
