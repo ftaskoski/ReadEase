0334f04b12c7ee2f6bddc4c5df4e6cc93df7a1e4
ftaskoski123 <filip.taskoski69@gmail.com>
2024-05-12 16:25:24 +0200

    new search for user feature

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -25,6 +25,27 @@
   </Card>
     </div>
 
+    <div class="w-full max-w-md lg:max-w-xl">
+  <Card>
+    <p class="text-3xl font-semibold text-gray-900 flex justify-center pb-2">Search For User</p>
+     
+      <div class="relative w-full h-10 mt-2">
+        <input
+          required
+          v-model="searchQuery"
+          @input="handleInput"
+          class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+          placeholder=""
+        />
+        <label
+          class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:flex-shrink before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900"
+        >
+         Username
+        </label>
+      </div>
+      
+  </Card>
+</div>
 
 <div class="w-full max-w-md lg:max-w-xl">
   <Card>
@@ -110,14 +131,23 @@ const usersPerPage = ref<number>(10);
 const paginatedUsers = ref<any[]>([]);
 const categories = ref<any[]>([]);
 const selectedCategories = ref<any[]>([]);
+const searchQuery = ref<string>("");
+let debounceTimer = 0;
+
 const totalPages = computed(() => {
-  let totalUsers;
-
-  const filteredUsers = users.value.filter(user => user.role !== 'Admin');
-  totalUsers = filteredUsers.length;
-
-  return Math.ceil(totalUsers / usersPerPage.value);
+  if (searchQuery.value.trim() !== "") {
+    // If search query is not empty, perform filtering
+    const filteredUsers = users.value.filter(user => user.role !== 'Admin' && user.username.includes(searchQuery.value));
+    const totalUsers = filteredUsers.length;
+    return Math.ceil(totalUsers / usersPerPage.value);
+  } else {
+    // If search query is empty, use the original user count
+    const totalUsers = users.value.filter(user => user.role !== 'Admin').length;
+    return Math.ceil(totalUsers / usersPerPage.value);
+  }
 });
+
+
 
 const visiblePages = computed(() => {
   let start = currPage.value - Math.floor(windowSize.value / 2);
@@ -221,6 +251,35 @@ function deleteCategories() {
       getAllCategories();
     });
 }
+
+function searchUser() {
+  axios
+    .get(`${url}api/searchusers`, {
+      withCredentials: true,
+      params: {
+        search: searchQuery.value,
+      },
+    })
+    .then((response) => {
+      paginatedUsers.value = response.data;
+     
+    });
+}
+
+function handleInput() {
+  currPage.value = 1;
+  clearTimeout(debounceTimer);
+  if (searchQuery.value.trim() !== "") {
+  
+    debounceTimer = setTimeout(() => {
+      searchUser();
+      
+    }, 1000);
+  } else {
+    getPaginatedUsers();
+  }
+}
+
 
 onMounted(() => {
   getUser();
