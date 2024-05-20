2b88a0acecf9e215802a982355fa9c0fb686a0cc
Filip Tashkoski <filip.taskoski69@gmail.com>
2024-05-20 13:43:05 +0200

    new delete modal

--- src/components/DeleteModal.vue
+++ src/components/DeleteModal.vue
@@ -1,45 +1,33 @@
 <template>
-    <div>
+   <div>
         <div v-if="showModal" class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-      <div class="relative w-auto max-w-md mx-auto my-6">
-        <!-- Content -->
-        <div class="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
-          <!-- Header -->
-          <div class="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
-            <h3 class="text-3xl font-semibold">
-              Confirm Delete
-            </h3>
-            <button
-              class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-            >
-              <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">×</span>
-            </button>
-          </div>
-          <!-- Body -->
-          <div class="relative p-6 flex-auto">
-            <p class="my-4 text-blueGray-500 text-lg leading-relaxed">
-              {{bodyText}}
-            </p>
-          </div>
-          <!-- Footer -->
-          <div class="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
-            <button
-            @click="confirmDelete()"
-              class="text-red-500 rounded-full hover:bg-gray-200 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              Delete
-            </button>
-            <button
-            @click="closeModal()"
-              class="text-blueGray-500 rounded-full hover:bg-gray-200 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      </div>
+          <div class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div class="fixed inset-0 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+  <div class="bg-white rounded-lg shadow-lg w-11/12 sm:w-1/3 md:w-1/2 inset-0 ">
+    <div class="px-6 py-4 border-b border-gray-200">
+      <h3 class="text-lg font-semibold text-gray-800">Confirm Delete</h3>
+    </div>
+    <div class="p-6">
+      <p class="text-gray-600">{{ bodyText }}</p>
+    </div>
+    <div class="px-6 py-4 border-t border-gray-200 flex justify-end space-x-4">
+      <button
+        @click="closeModal()"
+        class="px-4 py-2 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-400 hover:bg-gradient-to-br text-gray-800 rounded  focus:outline-none focus:ring-2 focus:ring-gray-500"
+      >
+        Cancel
+      </button>
+      <button
+      @click="confirmDelete()"
+        class="px-4 py-2 bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br text-white rounded  focus:outline-none focus:ring-2 focus:ring-red-500"
+      >
+        Delete
+      </button>
+    </div>
+  </div>
+</div>
+</div>
+
     </div>
     </div>
 </template>
