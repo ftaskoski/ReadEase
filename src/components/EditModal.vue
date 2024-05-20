e0036673668ec1a808d8fc0038f3992852ae7eb7
Filip Tashkoski <filip.taskoski69@gmail.com>
2024-05-20 14:03:11 +0200

    new edit modal ui

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -1,55 +1,55 @@
 <template>
     <div>
         <div v-if="showEditModal" class="fixed  inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-      <div class="relative w-auto max-w-md  mx-auto my-6">
-        <!-- Content -->
-        <div class="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
-          <!-- Header -->
-          <div class="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
-            <h3 class="text-3xl font-semibold">
-             Edit book
-            </h3>
-            <button
-              class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-            >
-              <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">×</span>
-            </button>
-          </div>
-          <!-- Body -->
-          <div class="relative p-6 flex-auto">  
-            <label class="text-gray-700 text-sm font-bold mb-2">Title</label>
-            <input type="text"  :value="title"  @input="$emit('update:newTitle', ($event.target as HTMLInputElement).value)" class="mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Title" >
-            <label class="text-gray-700 text-sm font-bold mb-2">Author</label>
-            <input type="text" :value="author" @input="$emit('update:newAuthor', ($event.target as HTMLInputElement).value)" class="mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Author">
-            <label class="text-gray-700 text-sm font-bold mb-2">Category</label>
-            <select :value="categoryId" @change="$emit('update:newCategoryId', ($event.target as HTMLInputElement).value)"  class="mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-              <option value="0" disabled selected hidden>Select a Category</option>
-              <option value="0" disabled>Select category</option>
-              <option  v-for="category in categories" :key="category.categoryId" :value="category.categoryId">{{ category.categoryName }}</option>
-            </select>
-          <div class="pointer-events-none absolute text-sm inset-y-0 right-6 top-[165px] flex items-center px-2 text-gray-700">
-              <span class="flex items-center">&#9660;</span>
-          </div>
-          </div>
-          <!-- Footer -->
-          <div class="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
-            <button
-              class="text-blue-500 rounded-full hover:bg-gray-200 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-              @click="updateBook()"   
-            >
-              Update
-            </button>
-            <button
-            @click="closeEditModal()"
-              class="text-blueGray-500 rounded-full hover:bg-gray-200 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
+          <div class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+    <div class="bg-white rounded-lg shadow-lg w-11/12 sm:w-1/3 md:w-1/2 inset-0 ">
+    <div class="px-6 py-4 border-b border-gray-200">
+      <h3 class="text-lg font-semibold text-gray-800">Edit Book</h3>
+    </div>
+    <div class="p-6 space-y-4">
+      <div>
+        <label class="block text-gray-700 font-medium mb-1">Title</label>
+        <input
+          :value="title" @input="$emit('update:newTitle', ($event.target as HTMLInputElement).value)"
+          type="text" class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+        />
       </div>
+      <div>
+        <label class="block text-gray-700 font-medium mb-1">Author</label>
+        <input
+          :value="author" @input="$emit('update:newAuthor', ($event.target as HTMLInputElement).value)"
+          type="text"
+          class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+        />
+      </div>
+      <div>
+        <label class="block text-gray-700 font-medium mb-1">Category</label>
+        <select
+        :value="categoryId" @change="$emit('update:newCategoryId', ($event.target as HTMLInputElement).value)"
+          class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+        >
+        <option value="0" disabled selected hidden>Select a Category</option>
+        <option value="0" disabled>Select category</option>
+        <option  v-for="category in categories" :key="category.categoryId" :value="category.categoryId">{{ category.categoryName }}</option>        
+      </select>
+      </div>
+    </div>
+    <div class="px-6 py-4 border-t border-gray-200 flex justify-end space-x-4">
+      <button
+      @click="closeEditModal()"
+        class="px-4 py-2 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-400 hover:bg-gradient-to-br text-gray-800 rounded  focus:outline-none focus:ring-2 focus:ring-gray-500"
+      >
+        Cancel
+      </button>
+      <button
+      @click="updateBook()"   
+        class="px-4 py-2 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br"
+      >
+        Update
+      </button>
+    </div>
+  </div>
+</div>
     </div>
     </div>
 </template>
