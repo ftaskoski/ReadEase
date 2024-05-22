5c7d874d6ea86209dedbd5f1dabb05d570c588e3
ftaskoski123 <filip.taskoski69@gmail.com>
2024-05-22 09:03:02 +0200

    new ui for the best sellers page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
-   <div class="order-1 sm:order-2 sm:ml-64 p-4 m">
-    <h1 class="flex justify-center items-center text-3xl font-semibold text-gray-900">New York Times</h1>
-  <div class="relative lg:ml-16 group flex flex-wrap justify-center items-center">
-    <div v-for="book in books" :key="book.rank" class="w-1/3 p-2">
-      <img :src="book.book_image" :alt="book.title" class="" />
+ <div class="order-1 sm:order-2 sm:ml-64 p-4 m">
+<div class="bg-gray-100 p-8">
+  <h2 class="text-3xl text-center mb-6 text-gray-800">This Week's Best Sellers - New York Times</h2>
+  <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+
+    <div  v-for="book in books" class="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:-translate-y-2">
+      <img :src="book.book_image"  class="w-full">
+      <div class="p-4 text-center">
+        <h3 class="text-xl text-gray-800 mb-2">{{book.title}}</h3>
+        <p class="text-gray-600">by {{book.author}}</p>
+      </div>
     </div>
   </div>
 </div>
+ </div>
 </template>
 
 <script setup lang="ts">
@@ -15,9 +22,9 @@ import axios from "axios";
 import {ref , onMounted } from 'vue';
 
 const books = ref<any[]>([]);
-const getnybooks = async () => {
-  const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=egM8BbGFHGODa7lpiV0SFCAKhJlzG72G`;
-  const response = await axios.get(url).then((response)=>{
+async function getnybooks () : Promise<void> {
+    const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=egM8BbGFHGODa7lpiV0SFCAKhJlzG72G`;
+    await axios.get(url).then((response)=>{
     books.value=response.data.results.books;
   })
 }
