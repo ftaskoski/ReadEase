86fbbbc9dfe03cc5d2105942803172e7f1bb2b3f
ftaskoski123 <filip.taskoski69@gmail.com>
2024-05-24 12:20:54 +0200

    removed shadow from select

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -37,7 +37,7 @@
         </label>
       </div>
       <div class="relative inline-block w-full mt-2 text-gray-500">
-        <select v-model="selectedCategory" required class="peer block appearance-none w-full border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500">
+        <select v-model="selectedCategory" required class="peer block appearance-none w-full border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded  leading-tight focus:outline-none focus:border-blue-500">
           <option value="" disabled selected hidden>Select a Category</option>
           <option v-for="category in categories" :key="category" :value="category">{{ category.categoryName }}</option>
         </select>
